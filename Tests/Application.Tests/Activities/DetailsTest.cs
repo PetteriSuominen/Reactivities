4590dec34083ac03fc4f6c4b55fa855ad68140bb
Petteri Suominen <petteri.suominen12@gmail.com>
2021-02-15 19:16:47 +0200

    Added GetActivityDetails test

--- Tests/Application.Tests/Activities/DetailsTest.cs
+++ Tests/Application.Tests/Activities/DetailsTest.cs
@@ -1,9 +1,11 @@
 ﻿using Application.Activities;
 using Application.Errors;
 using AutoMapper;
+using Domain;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using Persistence;
+using System;
 using System.Threading;
 using Xunit;
 
@@ -32,6 +34,30 @@ namespace Application.Tests.Activities
             //Act and Assert
             var exception = await Assert.ThrowsAsync<RestException>(() => handler.Handle(query, CancellationToken.None));
             Assert.Equal(System.Net.HttpStatusCode.NotFound, exception.Code);
+        }
+
+        [Fact]
+        public async void GetActivityDetails_ActivityFound_ReturnActivityDetails()
+        {
+            var activityId = Guid.NewGuid();
+
+            var activity = new Activity
+            {
+                Id = activityId,
+                Title = "Test activity"
+            };
+
+            _context.Activities.Add(activity);
+
+            _context.SaveChanges();
+
+           var handler = new Details.Handler(_context, _mapper.Object);
+
+            _mapper.Setup(a => a.Map<object, object>(It.IsAny<object>())).Returns(new ActivityDto { Id = activityId, Title = activity.Title });
+
+            var result = await handler.Handle(new Details.Query { Id = activityId }, CancellationToken.None);
+            Assert.Equal(activityId, result.Id);
+            Assert.Equal(activity.Title, result.Title);
         }
     }
 }
